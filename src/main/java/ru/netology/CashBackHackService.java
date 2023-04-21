a73a0f124032367ba218a70e9127dca7437353bd
OlgaMed <mama2482@mail.ru>
2023-04-21 20:22:14 +0400

    testng

--- src/main/java/ru/netology/CashBackHackService.java
+++ src/main/java/ru/netology/CashBackHackService.java
@@ -1,10 +1,10 @@
 package ru.netology;
 public class CashBackHackService {
 
-    private final int boundary = 1000;
+        private final int boundary = 1000;
 
-    public int remain(int amount) {
-        return boundary - amount % boundary;
+        public int remain(int amount) {
+            return boundary - amount % boundary;
+        }
     }
-}
 
--- src/test/java/ru/netology/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CashBackHackServiceTest.java
@@ -1,4 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
+package ru.netology;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
 class CashBackHackServiceTest {
-  
+
+    @Test
+    void remain600() {    //метод
+
+        CashBackHackService cashBack = new CashBackHackService();
+        int amount = 600;
+
+        int actual = cashBack.remain(amount);
+        int expected = 400;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    void remain999() {    //метод
+
+        CashBackHackService cashBack = new CashBackHackService();
+        int amount = 999;
+
+        int actual = cashBack.remain(amount);
+        int expected = 1;
+
+    }
 }
